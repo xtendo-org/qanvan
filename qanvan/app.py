d4bcff80747c7073f0dade084db3873a1192a10c
kinoru <xkinoru@gmail.com>
2016-01-16 15:50:50 +0900

    Add API: /list (POST)
    New API endpoint.
    
    Sending a POST request to /list with a JSON data, such as:
    
         {"name": "something"}
    
    will create a new card list.

--- qanvan/app.py
+++ qanvan/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
-from qanvan.models import db
+from flask import Flask, request, jsonify, abort
+from qanvan.models import db, CardList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+
+
+@app.route('/list', methods=['POST'])
+def list():
+    if request.method == 'POST':
+        # 새로운 카드리스트를 만듭니다.
+        name = request.get_json()['name']
+        # 'name'은 필수 필드
+        if 'name' is None:
+            abort(400)
+        cardList = CardList(name)
+        db.session.add(cardList)
+        db.session.commit()
+        return jsonify(result='ok')
+    # TODO: request.method == 'GET'
